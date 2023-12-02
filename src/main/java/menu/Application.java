c3ed9164c36d14c21061f88b3fbddb0c8897a1e7
Dongwoongkim <rlaehddnd0422@naver.com>
2023-12-02 20:07:43 +0900

    docs : 요구사항 정리 작성

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,12 @@
 package menu;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String s = Console.readLine();
+        String b = Console.readLine();
+        System.out.println(s + b);
     }
 }
