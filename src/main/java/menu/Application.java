236430368bc2210f375d676d148c3722ec2f0a60
Dongwoongkim <rlaehddnd0422@naver.com>
2023-12-02 20:16:03 +0900

    feat : 코치 이름 , 먹지 못하는 메뉴 입력 기능 구현

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,12 +1,13 @@
 package menu;
 
 import camp.nextstep.edu.missionutils.Console;
+import menu.controller.RecommendController;
+import menu.view.InputView;
+import menu.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        String s = Console.readLine();
-        String b = Console.readLine();
-        System.out.println(s + b);
+        RecommendController recommendController = new RecommendController(new InputView(), new OutputView());
+        recommendController.run();
     }
 }
---
+++ src/main/java/menu/controller/RecommendController.java
@@ -0,0 +1,20 @@
+package menu.controller;
+
+import menu.util.InputValidator;
+import menu.view.InputView;
+import menu.view.OutputView;
+
+public class RecommendController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public RecommendController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+
+    }
+}
